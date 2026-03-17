0734b248d9f04d688c744af418ccfa424a74440f
chaturprani114 <pranav.chaturvedi@lumberfi.com>
2026-03-17 07:13:44 +0530

    Add article fetching and table population

---
+++ script.js
@@ -0,0 +1,22 @@
+fetch("data/articles.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const table = document.getElementById("article-table");
+
+    data.forEach(article => {
+
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+        <td>${article.date}</td>
+        <td>${article.title}</td>
+        <td>${article.author}</td>
+        <td><a href="${article.link}" target="_blank">Read</a></td>
+      `;
+
+      table.appendChild(row);
+
+    });
+
+  });
